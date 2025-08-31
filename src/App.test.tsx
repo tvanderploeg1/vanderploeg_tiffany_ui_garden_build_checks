d78c03000df483b84872d21cffa9a53210081a62
Tiffany <tvanderploeg@rrc.ca>
2025-08-31 18:12:21 -0500

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders component library title', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const titleElement = screen.getByText(/UC Component Library/i);
-  expect(titleElement).toBeInTheDocument();
-});
-
-test('renders assignment info', () => {
-  render(<App />);
-  const assignmentElement = screen.getByText(/Assignment 13/i);
-  expect(assignmentElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
